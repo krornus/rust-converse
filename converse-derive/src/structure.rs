6afe37097b2d31c7ac7bb66881f3ce27c61746a9
spowell <spowell>
2019-01-17 11:43:53 -0500

    structure update, beginning implementing in client

--- converse-derive/src/structure.rs
+++ converse-derive/src/structure.rs
@@ -1,26 +1,34 @@
+
 use quote::quote;
 use proc_macro2::TokenStream;
-use syn::{Attribute, FnDecl, LifetimeDef, Ident, ImplItem, ImplItemMethod, ItemImpl, Visibility, WhereClause};
+
+use syn::{
+    Attribute, FnArg, FnDecl, GenericParam, LifetimeDef,
+    Ident, ImplItem, ImplItemMethod, ItemImpl, Pat,
+    ReturnType, Type, Visibility, WhereClause
+};
 use syn::token::{Comma, Default};
-use syn::punctuated::Punctuated;
+use syn::punctuated::{Pair, Punctuated};
 
 pub struct Structure {
     ident: Ident,
     generics: PhantomGenerics,
     members: Punctuated<Ident, TokenStream>,
     imp: Implementation,
+    parent: ItemImpl,
     phantoms: Vec<Ident>,
     autovar: usize,
 }
 
 impl Structure {
-    pub fn from_impl<'a>(ident: Ident, imp: &'a ItemImpl) -> Self {
+    pub fn from_impl<'a>(ident: Ident, imp: ItemImpl) -> Self {
 
         let mut structure = Structure {
             ident: ident.clone(),
-            generics: PhantomGenerics::from_impl(imp),
+            generics: PhantomGenerics::from_impl(&imp),
             members: Punctuated::new(),
-            imp: Implementation::from_impl(ident, imp),
+            imp: Implementation::from_impl(ident, &imp),
+            parent: imp,
             phantoms: vec![],
             autovar: 0,
         };
@@ -29,6 +37,20 @@ impl Structure {
         structure.phantoms = phantoms;
 
         structure
+    }
+
+    pub fn implement_parent(&self, body: TokenStream) -> TokenStream {
+        let attrs: TokenStream = self.parent.attrs.iter().map(|x| quote!{ #x }).collect();
+        let defaultness = &self.parent.defaultness;
+        let unsafety = &self.parent.unsafety;
+        let generics = &self.parent.generics;
+        let ty = &self.parent.self_ty;
+
+        quote! {
+            #attrs impl #defaultness #unsafety #generics #ty {
+                #body
+            }
+        }
     }
 
     pub fn declare(&self) -> TokenStream {
@@ -53,21 +75,16 @@ impl Structure {
         }
     }
 
-    pub fn implement<F>(&self, cb: F) -> TokenStream
-    where
-        F: Fn(FnDecl) -> TokenStream
-    {
+    pub fn implement(&self, body: TokenStream) -> TokenStream {
 
         let ident = &self.ident;
 
         let gen_decls = self.generics.decls();
         let gen_params = self.generics.params();
 
-        let functions: TokenStream = self.imp.functions(cb).into_iter().collect();
-
         quote! {
-            impl #gen_decls #ident #gen_params {
-                #functions
+            impl < #gen_decls > #ident < #gen_params > {
+                #body
             }
         }
     }
@@ -83,7 +100,7 @@ struct Implementation {
     attrs: Vec<Attribute>,
     ident: Ident,
     generics: PhantomGenerics,
-    methods: Vec<ImplItemMethod>,
+    methods: Vec<Method>,
 }
 
 impl Implementation {
@@ -93,7 +110,8 @@ impl Implementation {
             .filter_map(|x| match x {
                 ImplItem::Method(x) => Some(x.clone()),
                 _ => None,
-            }).collect();
+            })
+            .map(|x| Method::new(imp.self_ty.clone(), x)).collect();
 
         Implementation {
             attrs: imp.attrs.clone(),
@@ -103,11 +121,108 @@ impl Implementation {
         }
     }
 
-    fn functions<F>(&self, cb: F) -> Vec<TokenStream>
-    where
-        F: Fn(FnDecl) -> TokenStream
-    {
-        vec![]
+    fn methods(&self) -> &Vec<Method> {
+        &self.methods
+    }
+}
+
+struct Method {
+    ty: Box<Type>,
+    method: ImplItemMethod,
+}
+
+impl Method {
+    fn new(ty: Box<Type>, method: ImplItemMethod) -> Self {
+        Method {
+            ty: ty,
+            method: method,
+        }
+    }
+
+    /* Check if this is an instance method */
+    fn is_static(&self) -> bool {
+        if let Some(pair) = self.method.sig.decl.inputs.first() {
+            match pair.value() {
+                FnArg::SelfRef(_) => false,
+                FnArg::SelfValue(_) => false,
+                _ => true,
+            }
+        } else {
+            true
+        }
+    }
+
+    /* Get a list of arguments to the function - ignore self */
+    fn args(&self) -> Punctuated<Pat, Comma> {
+        self.method.sig.decl.inputs.pairs()
+            .filter(|x| match x.value() {
+                FnArg::SelfRef(_) => false,
+                FnArg::SelfValue(_) => false,
+                _ => true,
+            })
+            .filter_map(|x| {
+                match x.value() {
+                    FnArg::Captured(arg) => Some(Pair::new(
+                        arg.pat.clone(),
+                        x.punct().map(|x| (**x).clone())
+                    )),
+                    FnArg::Inferred(x) => {
+                        eprintln!("warning: ingored inferred variable {}", quote!(x));
+                        None
+                    },
+                    FnArg::Ignored(x) => {
+                        eprintln!("warning: ingored variable {}", quote!(x));
+                        None
+                    },
+                    _ => None,
+                }
+            })
+            .collect()
+    }
+
+    /* Get the return type */
+    fn ret(&self) -> TokenStream {
+        match &self.method.sig.decl.output {
+            ReturnType::Default => quote! { () },
+            ReturnType::Type(_, x) => quote! { #x },
+        }
+    }
+
+    /* Create the function declaration stream */
+    fn decl(&self, ret: TokenStream) -> TokenStream {
+
+        let sig = &self.method.sig;
+        let decl = &sig.decl;
+
+        if decl.variadic.is_some() {
+            eprintln!("warning: variadic methods are not supported yet");
+            return quote!();
+        }
+
+        let vis = &self.method.vis;
+        let defaultness = &self.method.defaultness;
+        let constness = &sig.constness;
+        let unsafety = &sig.unsafety;
+        let asyncness = &sig.asyncness;
+        let abi = &sig.abi;
+        let ident = &sig.ident;
+        let generics = &decl.generics;
+        let inputs = &decl.inputs;
+
+        quote! { #vis #defaultness #constness #unsafety #asyncness #abi fn #ident #generics ( #inputs ) -> #ret }
+    }
+
+    /* Call the function with args */
+    fn call(&self, args: Punctuated<TokenStream, Comma>) -> TokenStream {
+
+        let ident = &self.method.sig.ident;
+
+        if self.is_static() {
+            let ty = &self.ty;
+            quote! { #ty::#ident(#args) }
+        } else {
+            quote! { self.state.#ident(#args) }
+        }
     }
 }
 
@@ -127,11 +242,11 @@ impl PhantomGenerics {
         /* track ids so we can check for conflicts */
         for gen in imp.generics.params.iter() {
             match gen {
-                syn::GenericParam::Type(x) => {
+                GenericParam::Type(x) => {
                     type_ids.push(&x.ident);
                     generics.push(PhantomGeneric::new(x.ident.clone(), None));
                 },
-                syn::GenericParam::Lifetime(x) => lifetimes.push(x),
+                GenericParam::Lifetime(x) => lifetimes.push(x),
                 _ => eprintln!("Constant generic types are currently unsupported for Converse"),
             }
         }
@@ -142,7 +257,7 @@ impl PhantomGenerics {
         while !lifetimes.is_empty() {
 
             let lifetime = lifetimes.pop().unwrap();
-            let id = syn::Ident::new(&format!("PhantomType_{}", ty_idx), proc_macro2::Span::call_site());
+            let id = Ident::new(&format!("PhantomType_{}", ty_idx), proc_macro2::Span::call_site());
 
             /* conflict */
             if type_ids.contains(&&id) {
